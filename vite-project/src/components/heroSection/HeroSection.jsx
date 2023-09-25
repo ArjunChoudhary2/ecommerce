1a5e87205bb39661a495c0c097c072122992b81a
ArjunChoudhary2 <105961121+ArjunChoudhary2@users.noreply.github.com>
2023-09-25 11:29:35 +0530

    Hero Image updated

--- vite-project/src/components/heroSection/HeroSection.jsx
+++ vite-project/src/components/heroSection/HeroSection.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import Image from '../../shopping-hero-page-cover-modern-with-shopping-cart-.png'
+import heroImage from '../../Images/online-shopping-store-simple-cover-.png'
 
 export default function HeroSection() {
   return (
     <div><img
-    src='https://static.vecteezy.com/system/resources/previews/004/299/835/original/online-shopping-on-phone-buy-sell-business-digital-web-banner-application-money-advertising-payment-ecommerce-illustration-search-free-vector.jpg' alt=''
+    src={heroImage}
+    // src='https://static.vecteezy.com/system/resources/previews/004/299/835/original/online-shopping-on-phone-buy-sell-business-digital-web-banner-application-money-advertising-payment-ecommerce-illustration-search-free-vector.jpg' alt=''
     /></div>
   )
 }
