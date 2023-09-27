bfc451354de6f7ff3ffa6500e5230500c1973705
ArjunChoudhary2 <105961121+ArjunChoudhary2@users.noreply.github.com>
2023-09-27 12:01:22 +0530

    Implemented add, update and delete product implemented. App hosted on Firebase.

--- vite-project/src/context/data/MyState.jsx
+++ vite-project/src/context/data/MyState.jsx
@@ -1,6 +1,19 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import myContext from "./myContext";
-
+import {
+  QuerySnapshot,
+  Timestamp,
+  addDoc,
+  collection,
+  onSnapshot,
+  orderBy,
+  query,
+  setDoc,
+  doc,
+  deleteDoc,
+} from "firebase/firestore";
+import { toast } from "react-toastify";
+import { fireDB } from "../../firebase/FireBaseConfig";
 export default function MyState(props) {
   const [theme, setTheme] = useState("light");
   const toggleTheme = () => {
@@ -14,10 +27,127 @@ export default function MyState(props) {
   };
 
   const [loading, setLoading] = useState(false);
-  
+
+  //ADDING PRODUCT TO THE DB
+  const [products, setProducts] = useState({
+    title: null,
+    price: null,
+    imageUrl: null,
+    category: null,
+    description: null,
+    time: Timestamp.now(),
+    date: new Date().toLocaleString("en-US", {
+      month: "short",
+      day: "2-digit",
+      year: "numeric",
+    }),
+  });
+
+  const addProduct = async () => {
+    if (
+      products.title === null ||
+      products.price === null ||
+      products.imageUrl === null ||
+      products.category === null ||
+      products.description === null
+    ) {
+      return toast.error("all feilds are required");
+    }
+    setLoading(true);
+    try {
+      const productRef = collection(fireDB, "products");
+      await addDoc(productRef, products);
+      toast.success("Product added successfully");
+      setTimeout(() => {
+        window.location.href = "/dashboard";
+      }, 800);
+      getProductData();
+
+      setLoading(false);
+    } catch (error) {
+      console.error(error);
+      setLoading(false);
+    }
+  };
+
+  //GETTING PRODUCT FROM THE DB
+  const [product, setProduct] = useState([]);
+
+  const getProductData = async () => {
+    setLoading(true);
+    try {
+      const queryDB = query(collection(fireDB, "products"), orderBy("time"));
+      const data = onSnapshot(queryDB, (QuerySnapshot) => {
+        let productArray = [];
+        QuerySnapshot.forEach((doc) => {
+          productArray.push({ ...doc.data(), id: doc.id });
+        });
+        setProduct(productArray);
+        setLoading(false);
+      });
+
+      return () => data;
+    } catch (error) {
+      console.error(error);
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getProductData();
+  }, []);
+
+  //UPDATE product
+
+  const handleUpdate = (item) => {
+    setProducts(item);
+  };
+
+  const updateProduct = async (item) => {
+    setLoading(true);
+    try {
+      await setDoc(doc(fireDB, "products", products.id), products);
+      toast.success("Product updated successfully");
+      getProductData();
+      setTimeout(() => {
+        window.location.href = "/dashboard";
+      },800) ;
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const deleteProduct = async (item) => {
+    setLoading(true);
+    try {
+      await deleteDoc(doc(fireDB, "products", item.id));
+      toast.success("Product deleted successfully");
+      getProductData();
+      setLoading(false);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   return (
-    <myContext.Provider value={{ theme, toggleTheme, loading, setLoading }}>
+    <myContext.Provider
+      value={{
+        product,
+        setProduct,
+        setProducts,
+        addProduct,
+        getProductData,
+        products,
+        theme,
+        toggleTheme,
+        loading,
+        setLoading,
+        handleUpdate,
+        deleteProduct,
+        updateProduct
+      }}
+    >
       {props.children}
     </myContext.Provider>
   );
--- vite-project/src/pages/admin/page/AddProduct.jsx
+++ vite-project/src/pages/admin/page/AddProduct.jsx
@@ -1,59 +1,92 @@
-import React from 'react'
+import React, { useContext } from "react";
+import myContext from "../../../context/data/myContext";
 
 function AddProduct() {
-    return (
-        <div>
-            <div className=' flex justify-center items-center h-screen'>
-                <div className=' bg-gray-800 px-10 py-10 rounded-xl '>
-                    <div className="">
-                        <h1 className='text-center text-white text-xl mb-4 font-bold'>Add Product</h1>
-                    </div>
-                    <div>
-                        <input type="text"
-                            name='title'
-                            className=' bg-gray-600 mb-4 px-2 py-2 w-full lg:w-[20em] rounded-lg text-white placeholder:text-gray-200 outline-none'
-                            placeholder='Product title'
-                        />
-                    </div>
-                    <div>
-                        <input type="text"
-                            name='price'
-                            className=' bg-gray-600 mb-4 px-2 py-2 w-full lg:w-[20em] rounded-lg text-white placeholder:text-gray-200 outline-none'
-                            placeholder='Product price'
-                        />
-                    </div>
-                    <div>
-                        <input type="text"
-                            name='imageurl'
-                            className=' bg-gray-600 mb-4 px-2 py-2 w-full lg:w-[20em] rounded-lg text-white placeholder:text-gray-200 outline-none'
-                            placeholder='Product imageUrl'
-                        />
-                    </div>
-                    <div>
-                        <input type="text"
-                            name='category'
-                            className=' bg-gray-600 mb-4 px-2 py-2 w-full lg:w-[20em] rounded-lg text-white placeholder:text-gray-200 outline-none'
-                            placeholder='Product category'
-                        />
-                    </div>
-                    <div>
-                       <textarea cols="30" rows="10" name='title'
-                            className=' bg-gray-600 mb-4 px-2 py-2 w-full lg:w-[20em] rounded-lg text-white placeholder:text-gray-200 outline-none'
-                            placeholder='Product title'>
+  const context = useContext(myContext);
+  const { setProducts, addProduct, products } = context;
 
-                       </textarea>
-                    </div>
-                    <div className=' flex justify-center mb-3'>
-                        <button
-                            className=' bg-yellow-500 w-full text-black font-bold  px-2 py-2 rounded-lg'>
-                            Add Product
-                        </button>
-                    </div>
-                 
-                </div>
-            </div>
+  return (
+    <div>
+      <div className=" flex justify-center items-center h-screen">
+        <div className=" bg-gray-800 px-10 py-10 rounded-xl ">
+          <div className="">
+            <h1 className="text-center text-white text-xl mb-4 font-bold">
+              Add Product
+            </h1>
+          </div>
+          <div>
+            <input
+              type="text"
+              name="title"
+              className=" bg-gray-600 mb-4 px-2 py-2 w-full lg:w-[20em] rounded-lg text-white placeholder:text-gray-200 outline-none"
+              placeholder="Product title"
+              value={products.title}
+              onChange={(e) => {
+                setProducts({ ...products, title: e.target.value });
+              }}
+            />
+          </div>
+          <div>
+            <input
+              type="text"
+              name="price"
+              className=" bg-gray-600 mb-4 px-2 py-2 w-full lg:w-[20em] rounded-lg text-white placeholder:text-gray-200 outline-none"
+              placeholder="Product price"
+              value={products.price}
+              onChange={(e) => {
+                setProducts({ ...products, price: e.target.value });
+              }}
+            />
+          </div>
+          <div>
+            <input
+              type="text"
+              name="imageurl"
+              className=" bg-gray-600 mb-4 px-2 py-2 w-full lg:w-[20em] rounded-lg text-white placeholder:text-gray-200 outline-none"
+              placeholder="Product imageUrl"
+              value={products.imageUrl}
+              onChange={(e) => {
+                setProducts({ ...products, imageUrl: e.target.value });
+              }}
+            />
+          </div>
+          <div>
+            <input
+              type="text"
+              name="category"
+              className=" bg-gray-600 mb-4 px-2 py-2 w-full lg:w-[20em] rounded-lg text-white placeholder:text-gray-200 outline-none"
+              placeholder="Product category"
+              value={products.category}
+              onChange={(e) => {
+                setProducts({ ...products, category: e.target.value });
+              }}
+            />
+          </div>
+          <div>
+            <textarea
+              cols="30"
+              rows="10"
+              name="description"
+              className=" bg-gray-600 mb-4 px-2 py-2 w-full lg:w-[20em] rounded-lg text-white placeholder:text-gray-200 outline-none"
+              placeholder="Product description"
+              value={products.description}
+              onChange={(e) => {
+                setProducts({ ...products, description: e.target.value });
+              }}
+            ></textarea>
+          </div>
+          <div className=" flex justify-center mb-3">
+            <button
+              onClick={addProduct}
+              className=" bg-yellow-500 w-full text-black font-bold  px-2 py-2 rounded-lg"
+            >
+              Add Product
+            </button>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
-export default AddProduct
+export default AddProduct;
